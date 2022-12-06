41352ce5e29ce1138b5ae128a0fa49b3b620868d
JeFFunique <94359049+JeFFunique@users.noreply.github.com>
2022-12-06 23:23:50 +0100

    Update cryptos.js

--- backend/routes/cryptos.js
+++ backend/routes/cryptos.js
@@ -5,7 +5,7 @@ router = express.Router()
 router.get("/crypto", getCryptos) //
 router.get("/:cryptoId", getCrypto) //
 router.post("/add", postCrypto) //
-router.put(":/cryptoId", updateCrypto)
+router.put("/:cryptoId", updateCrypto)
 router.delete("/:cryptoId", deleteCrypto)
 
 module.exports = router
