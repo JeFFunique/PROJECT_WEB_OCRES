af2cd576107811b1f3185cf78b9285e4973d6a3f
JeFFunique <94359049+JeFFunique@users.noreply.github.com>
2022-12-07 07:18:13 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,22 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import Axios from "axios"
+import { Line } from 'react-chartjs-2';
 import Coin from './components/Coin';
+import { Chart as ChartJS, Title, Tooltip, LineElement, Legend, CategoryScale, LinearScale, PointElement } from "chart.js"
+ChartJS.register(Title, Tooltip, LineElement, Legend, CategoryScale, LinearScale, PointElement);
 function App() {
   const [listOfCoins, setListOfCoins] = useState([]);
   const [searchWord, setSearchWord] = useState([]);
+  const [data, setData] = useState({
+    labels: ["bitcoin", "ethereum", "solana", "binance", "litecoin", "avalanche", "chainlick", "wrapped bitcoin"],
+    datasets: [{
+      label: "Crypto Repartition example",
+      data: [16972.25, 1249.99, 14.012, 288.52, 77.88, 13.53, 7.066, 16922.17],
+      backgroundColor: "rgb(255, 99, 132)",
+    }]
+  })
+
   useEffect(() => {
     Axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
       .then((response) => {
@@ -36,8 +48,9 @@ function App() {
         );
       })}
       </div>
-
-      
+  <div className='linech' style={{ width: "100%" }} />
+      <Line data={data} />
+      </div>
       </div>
     </div>
 
